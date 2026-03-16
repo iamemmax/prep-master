266b051099487937b114be7f1612ff240d31a8ed
iamemmax <iam4emmax@gmail.com>
2026-03-16 22:01:50 +0100

    update

--- components/home/how-it-works.tsx
+++ components/home/how-it-works.tsx
@@ -52,7 +52,7 @@ export default function HowItWorks() {
           </p>
         </MotionReveal>
 
-        <div className="mx-auto mt-10 grid max-w-375 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-auto mt-10 grid max-w-375  gap-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
           {STEPS.map((s, idx) => {
             // const Icon = s.icon
             return (
